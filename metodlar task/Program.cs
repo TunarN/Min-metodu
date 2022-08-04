4760139a9dd53b15853de6e9f80deffbe4d1863a
TunarN <tunarzn@code.edu.az>
2022-08-04 14:27:42 +0400

    Homework

---
+++ metodlar task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace metodlar_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 3, 5, 7, 8, 1 };
+            Console.WriteLine(Min(nums));
+        }
+        static int Min(int[] nums)
+        {
+            int min = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i]<min)
+                {
+                    min = nums[i];
+                }
+                
+            }
+            return min;
+
+        }
+    }
+}
